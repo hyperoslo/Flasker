e516442f21a466725247ad9d6932124f20195a2d
Khoa Pham <onmyway133@gmail.com>
2016-07-27 13:36:32 +0200

    Conform Int, Float, Double, NSURL to DataConvertible

--- Sources/DataConvertible.swift
+++ Sources/DataConvertible.swift
@@ -40,3 +40,51 @@ extension Bool: DataConvertible {
     return NSNumber.decode(data)?.boolValue
   }
 }
+
+extension Int: DataConvertible {
+
+  public func encode() -> NSData? {
+    return NSNumber(integer: self).encode()
+  }
+
+  public static func decode(data: NSData) -> Int? {
+    return NSNumber.decode(data)?.integerValue
+  }
+}
+
+extension Float: DataConvertible {
+
+  public func encode() -> NSData? {
+    return NSNumber(float: self).encode()
+  }
+
+  public static func decode(data: NSData) -> Float? {
+    return NSNumber.decode(data)?.floatValue
+  }
+}
+
+extension Double: DataConvertible {
+
+  public func encode() -> NSData? {
+    return NSNumber(double: self).encode()
+  }
+
+  public static func decode(data: NSData) -> Double? {
+    return NSNumber.decode(data)?.doubleValue
+  }
+}
+
+extension NSURL: DataConvertible {
+
+  public func encode() -> NSData? {
+    return absoluteString.encode()
+  }
+
+  public static func decode(data: NSData) -> NSURL? {
+    if let string = String.decode(data) {
+      return NSURL(string: string)
+    }
+
+    return nil
+  }
+}
