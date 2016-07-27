f0c7c729a2623a01a8d11cb0c738ad0df0e6db96
Khoa Pham <onmyway133@gmail.com>
2016-07-28 00:14:36 +0200

    Update Tests

--- FlaskerTests/Tests.swift
+++ FlaskerTests/Tests.swift
@@ -2,17 +2,38 @@ import XCTest
 import Flasker
 
 extension Key {
-  static let key = Key(rawValue: "key")
+  static let string = Key(rawValue: "string")
+  static let int = Key(rawValue: "int")
+  static let float = Key(rawValue: "float")
+  static let double = Key(rawValue: "double")
+  static let bool = Key(rawValue: "bool")
+  static let url = Key(rawValue: "url")
 }
 
 class Tests: XCTestCase {
 
-  func testExample() {
+  func testFlask() {
 
     let flask = Flask(userDefaults: NSUserDefaults.standardUserDefaults(), secret: "")
 
-    flask.set("hello", key: Key.key)
-    XCTAssertEqual(flask.get(Key.key), "hello")
+    flask.set("hello", key: Key.string)
+    XCTAssertEqual(flask.get(Key.string), "hello")
+
+    flask.set(10, key: Key.int)
+    XCTAssertEqual(flask.get(Key.int), 10)
+
+    flask.set(10.5, key: Key.float)
+    XCTAssertEqual(flask.get(Key.float), 10.5)
+
+    flask.set(10.10, key: Key.double)
+    XCTAssertEqual(flask.get(Key.double), 10.10)
+
+    flask.set(true, key: Key.bool)
+    XCTAssertEqual(flask.get(Key.bool), true)
+
+    let url = NSURL(string: "http://www.google.com")
+    flask.set(url!, key: Key.url)
+    XCTAssertEqual(flask.get(Key.url), url)
   }
 
 }
